22581f7fe0afd491f45b927a9154c2548bdd9249
JonathanDecleire <70289692+JonathanDecleire@users.noreply.github.com>
2020-11-09 12:15:46 +0100

    Create class Board and method start_game

--- utils/game.py
+++ utils/game.py
@@ -1,1 +1,13 @@
+class Board():
+    """
+    Class Board with its attributes and method
+    """
+    def __init__(self, players: str, turn_count: int, active_cards: , history_cards: ):
+        self.players =
+        self.turn_count =
+        self.active_cards =
+        self.history_cards =
 
+    #create a method start_game()
+    def start_game():
+        
