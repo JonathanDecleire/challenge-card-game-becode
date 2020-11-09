7e20f6dc15f31cee5ba285ba7f53f8be98ced1a4
JonathanDecleire <70289692+JonathanDecleire@users.noreply.github.com>
2020-11-09 14:30:06 +0100

    Update

--- utils/card.py
+++ utils/card.py
@@ -3,8 +3,8 @@ class Symbol:
     Class Symbol that defines the color and icon of the card
     """
     def __init__(self, color: str, icon: str):
-        self.color = [black, red]
-        self.icon = [♥, ♦, ♣, ♠]
+        self.color = ["black", "red"]
+        self.icon = ["♥", "♦", "♣", "♠"]
 
     def __str__(self):
         return " A {self.color} {self.icon}".format(self=self)
@@ -14,9 +14,9 @@ class Card(Symbol):
     Class Card inherits from Class Symbol
     """
     def __init__(self, color: str, icon: str, value: str):
-        self.color = [black, red]
-        self.icon = [♥, ♦, ♣, ♠]
-        self.value = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+        self.color = ["black", "red"]
+        self.icon = ["♥", "♦", "♣", "♠"]
+        self.value = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
     def __str__(self):
         return " A {self.color} {self.icon} {self.value}".format(self=self)
