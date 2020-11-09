87b449906534d0e18bf657386ba97b60a81d7edf
JonathanDecleire <70289692+JonathanDecleire@users.noreply.github.com>
2020-11-09 11:38:41 +0100

    Initialize class Player with attributes and method play()

--- utils/player.py
+++ utils/player.py
@@ -1,1 +1,19 @@
+import Card from card.py
 
+class Player:
+    """
+    Class Player which sets up the attributes of each player
+    """
+    def __init__(self, cards: str, turn_count: int, number_of_cards: int, history: str):
+        turn_count = 0
+        number_of_cards = 0
+        self.cards =
+        self.turn_out =
+        self.number_of_cards =
+        self.history =
+
+    def __str__(self):
+        return " A {self.color} {self.icon}".format(self=self)
+
+    #create a method play()
+    def play(self):
