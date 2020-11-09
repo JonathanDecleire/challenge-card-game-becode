bf9455b058882c0ee4651fcf6f28172067bc46e5
JonathanDecleire <70289692+JonathanDecleire@users.noreply.github.com>
2020-11-09 10:48:56 +0100

    Adding class Symbol

--- utils/card.py
+++ utils/card.py
@@ -1,1 +1,10 @@
+class Symbol:
+    """
+    Class Symbol that defines the color and icon of the card
+    """
+    def __init__(self, color: str, icon: str):
+        self.color = [black, red]
+        self.icon = [♥, ♦, ♣, ♠]
 
+    def __str__(self):
+        return " A {self.color} {self.icon}".format(self=self)
