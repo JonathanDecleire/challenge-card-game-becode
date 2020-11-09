a95ff7697ca15acdb047b68481bab5ccfb311828
JonathanDecleire <70289692+JonathanDecleire@users.noreply.github.com>
2020-11-09 11:01:17 +0100

    Added class Card inherited from class Symbol

--- utils/card.py
+++ utils/card.py
@@ -8,3 +8,15 @@ class Symbol:
 
     def __str__(self):
         return " A {self.color} {self.icon}".format(self=self)
+
+class Card(Symbol):
+    """
+    Class Card inherits from Class Symbol
+    """
+    def __init__(self, color: str, icon: str, value: str):
+        self.color = [black, red]
+        self.icon = [♥, ♦, ♣, ♠]
+        self.value = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+
+    def __str__(self):
+        return " A {self.color} {self.icon} {self.value}".format(self=self)
